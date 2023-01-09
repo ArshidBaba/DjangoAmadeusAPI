76d69862fa0af403cf39bad67e8085e9f51426c7
Arshid <arshidbaba93@gmail.com>
2023-01-09 13:07:55 +0530

    Aded Flight search based on origin and destination

--- flight/urls.py
+++ flight/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("select_destination/<str:param>/", views.select_destination, name="select-destination"),
+    path("search_offers/", views.search_offers, name="search_offers"),
 ]
--- flight/views.py
+++ flight/views.py
@@ -23,3 +23,23 @@ def select_destination(req, param):
     else:
         return JsonResponse({"error": "Invalid request method"})
 
+
+def search_offers(req):
+    if req.method == "GET":
+        try:
+            origin_code = req.GET["originCode"]
+            destination_code = req.GET["destinationCode"]
+            departure_date = req.GET["departureDate"]
+            response = amadeus.shopping.flight_offers_search.get(
+                originLocationCode=origin_code,
+                destinationLocationCode=destination_code,
+                departureDate=departure_date, adults=1    
+            )
+            context = {
+                "data": response.data
+            }
+            return JsonResponse(context)
+        except ResponseError as error:
+            print(error)
+    else:
+        return JsonResponse({"error": "Invalid request method"})
