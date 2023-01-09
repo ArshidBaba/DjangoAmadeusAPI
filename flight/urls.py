cc661f3dd93ac6bc4363eeec108815573e9206bd
Arshid <arshidbaba93@gmail.com>
2023-01-09 12:00:37 +0530

    Created Search Flights endpoint based on a city

---
+++ flight/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("select_destination/<str:param>/", views.select_destination, name="select-destination"),
+]
--- flight/views.py
+++ flight/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from amadeus import Client, ResponseError, Location
+from django.http import JsonResponse
 
-# Create your views here.
+amadeus = Client(
+    client_id = "aRUhXmiqALwk9IT5nZeGk6Ciw49cMkPd",
+    client_secret = "ko7wEPGTeAcoABge"
+)
+
+
+def select_destination(req, param):
+    if req.method == "GET":
+        try:
+            print(param)
+            response = amadeus.reference_data.locations.get(
+                keyword=param, subType=Location.ANY
+            )
+            context = {
+                "data": response.data
+            }
+            return JsonResponse(context)
+        except ResponseError as error:
+            print(error)
+    else:
+        return JsonResponse({"error": "Invalid request method"})
+
